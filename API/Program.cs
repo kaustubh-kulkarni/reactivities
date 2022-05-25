a8c78b2c709a68599fa57462a001b90b257483db
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2022-05-25 23:08:28 +0200

    Fixed untracked files

--- API/Program.cs
+++ API/Program.cs
@@ -3,17 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            //Scope is going to be disposed off by the thread
+            using var scope = host.Services.CreateScope();
+            //calling the methods on scope
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                //Using service locator pattern to populate the data context
+                var context = services.GetRequiredService<DataContext>();
+                //Apply pending migrations and will create DB if does not exist
+                await context.Database.MigrateAsync();
+                //Seed the data
+                await Seed.SeedData(context);
+            }
+            catch(Exception ex)
+            {
+                //In case of an exception we create a variable logger in context of ILogger service
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                //Here we pass the exception to logger
+                logger.LogError(ex, "An error occured during migration");
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
