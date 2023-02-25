a6ee4594f9e649109a1f756108991d0126b5c909
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2023-02-25 18:00:16 +0100

    Added initial migration and created code first migration in program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//DbContext service
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -16,6 +23,26 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthorization();
 app.MapControllers();
+
+//When we are done with this scope everything is gonna get destroyed and free memory
+using var scope = app.Services.CreateScope();
+//Another var to scope to service provider
+var services = scope.ServiceProvider;
+
+try
+{
+    //Here need to get the DataContext service in order to perform the migration
+    var context = services.GetRequiredService<DataContext>();
+    //Migrate the DB
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{
+    //Creating a logger instance within the scope of program.cs to report logs
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration!");
+}
 
 app.Run();
